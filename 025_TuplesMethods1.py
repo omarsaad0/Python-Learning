6770113449f232cffec5dccf4844b2f8def6cb66
Omar Saad <omarsaad2411@gmail.com>
2021-08-28 20:57:54 +0200

    .

---
+++ 025_TuplesMethods1.py
@@ -0,0 +1,20 @@
+# Tuples ##################################
+
+my_tuple = ("Omar", "Mohammed")
+my_tuple2 = "Omar", "Mohammed"
+
+print(my_tuple)
+print(my_tuple2)
+print(type(my_tuple))
+print(type(my_tuple2))
+
+# Tuple Indexing ##################################
+my_tuple3 = (1, 2, 3, 4, 5)
+print(my_tuple3[0])  # Indexing Same as list
+
+# Tuple Assign Values ##################################
+
+my_tuple4  = (1, 2, 3, 5)
+#my_tuple4[2] = "Three"  #TypeError: 'tuple' object does not support item assignment
+
+# Tuple Items ##################################
