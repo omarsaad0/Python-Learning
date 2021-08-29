846ae425bbcf63210ee5e1f57b6a73d4f694980f
Omar Saad <omarsaad2411@gmail.com>
2021-08-29 08:51:31 +0200

    .

--- 025_TuplesMethods1.py
+++ 025_TuplesMethods1.py
@@ -18,3 +18,75 @@ my_tuple4  = (1, 2, 3, 5)
 #my_tuple4[2] = "Three"  #TypeError: 'tuple' object does not support item assignment
 
 # Tuple Items ##################################
+
+# Tuple with one element
+
+my_tuple5 = ("Omar",)  # , makes a tuble
+my_string = ("Omar")
+
+print(type(my_tuple))
+print(type(my_string))
+
+# Tuple Concatenation
+
+a = (1, 2, 3, 4, 5)
+b = (5, 6)
+c = a + b
+d = a + ("A", "B", True) + c
+print(c)
+print(d)
+
+# Tuple, List, String Repeat (*)
+
+mystring = "omar"
+myList = [1, 2]
+my_tuple6 = (1, 2)
+print(mystring*5)
+
+# Method => count()
+a = (1, 3, 4, 5, 1, 6, 5, 1)
+print(a.count(1))
+
+# Method => index()
+
+b = (1, 3, 7, 5, 6)
+print("The Position of Index is: {:d}".format(b.index(5)))
+print(f"The Position of Index is: {b.index(5)}")
+
+# Tuple Destruct
+
+a = ("A", "B", "C")
+#x, y, z = "A", "B", "C"  # or another way
+x, y, z = a  # Packing the tuple to 3 variable
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
