de82782e69886ada473a388a2d56242adf826c10
Omar Saad <omarsaad2411@gmail.com>
2021-08-30 11:18:00 +0200

    .

---
+++ 035_AssignmentOperators.py
@@ -0,0 +1,17 @@
+# Assignment Operators
+
+# =
+# +=
+# -+
+# *=
+# /=
+# **=
+# %=
+# //=
+
+x = 10  # var one
+y = 20  # var one
+#x = x + y  # var one = self [operator +] var two
+#x += y
+x -=y
+print(x)
