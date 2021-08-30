acd402b39931f60de42c093a84aa4cb780b83404
Omar Saad <omarsaad2411@gmail.com>
2021-08-30 11:06:26 +0200

    .

---
+++ 033_Boolean.py
@@ -0,0 +1,30 @@
+# Boolean
+
+name = ""
+print(name.isspace())
+
+print("/|"*50)
+
+print(100 > 200)
+print(100 > 90)
+print(100 > 100)
+
+print("/|"*50)
+
+# True Values ##################################################################################
+# Not Empty Values
+print(bool("Omar"))
+print(bool(100))
+print(bool(100.95))
+print(bool(True))
+print(bool([1, 2, 3]))
+print("/|"*50)
+# False Values ##################################################################################
+# Empty values
+print(bool(0))
+print(bool(False))
+print(bool(""))
+print(bool([]))
+print(bool(()))
+print(bool({}))
+print(bool(None))
