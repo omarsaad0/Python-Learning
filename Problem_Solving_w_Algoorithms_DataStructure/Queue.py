4c9e068b7b9bbd83cc377bb7fd64ea300d07c8e3
Omar Saad <omarsaad2411@gmail.com>
2021-09-14 23:45:16 +0200

    .

--- Problem_Solving_w_Algoorithms_DataStructure/Queue.py
+++ Problem_Solving_w_Algoorithms_DataStructure/Queue.py
@@ -20,9 +20,29 @@ class Queue:
 
 
 test = Queue()
-
+'''
 test.enqueue('hello')
 test.enqueue('dog')
 test.enqueue(3)
 test.dequeue()
 print(test.look())
+'''
+
+def hot_potato(name_list, num):
+
+    hot_queue = Queue()
+
+    for i in name_list:
+        hot_queue.enqueue(i)
+
+    while hot_queue.size()>1:
+
+        for i in range(num):
+            removed = hot_queue.dequeue()
+            hot_queue.enqueue(removed)
+        hot_queue.dequeue()
+
+    return hot_queue.look()
+
+
+print(hot_potato(["Omar", "Salah", "Fekry", "Haitham", "Nabawy", "Eman", "Mahmoud", "Adham"], 9))
