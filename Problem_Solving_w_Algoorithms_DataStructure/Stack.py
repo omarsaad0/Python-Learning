0310aff3edf27d3a1c42b50ec2f8f2e224431584
Omar Saad <omarsaad2411@gmail.com>
2021-09-14 14:42:16 +0200

    .

---
+++ Problem_Solving_w_Algoorithms_DataStructure/Stack.py
@@ -0,0 +1,31 @@
+class Stack():
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)    # .insert will be used with index 0 if we choose the end is the beginning of the list
+        print(f"{item} is added to the stack")
+
+    def pop(self):
+        return self.items.pop()    # Index 0 if we chose the end is at beginning of the list
+
+    def peek(self):
+        return self.items[len(self.items)-1]  # Index 0 if we ....
+
+    def size(self):
+        return len(self.items)
+
+
+s = Stack()
+
+s.push(5)
+s.push("Hi")
+s.push("Test")
+s.pop()
+s.peek()
+print(s.size())
+print(s.is_empty())
