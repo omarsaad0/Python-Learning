f7eac3b7c56725b9417ad75d1704a2e1ccece2fc
Omar Saad <omarsaad2411@gmail.com>
2021-09-14 14:56:47 +0200

    .

--- Problem_Solving_w_Algoorithms_DataStructure/Stack.py
+++ Problem_Solving_w_Algoorithms_DataStructure/Stack.py
@@ -8,7 +8,7 @@ class Stack():
 
     def push(self, item):
         self.items.append(item)    # .insert will be used with index 0 if we choose the end is the beginning of the list
-        print(f"{item} is added to the stack")
+        #print(f"{item} is added to the stack")
 
     def pop(self):
         return self.items.pop()    # Index 0 if we chose the end is at beginning of the list
@@ -21,7 +21,7 @@ class Stack():
 
 
 s = Stack()
-
+'''
 s.push(5)
 s.push("Hi")
 s.push("Test")
@@ -29,3 +29,22 @@ s.pop()
 s.peek()
 print(s.size())
 print(s.is_empty())
+'''
+########################################################################################################################
+
+
+def par_checker (symbol_string):
+    balanced = True
+    for i in symbol_string:
+
+        if i == '(':
+            s.push(i)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                s.pop()
+
+    return print(f'Is The String Balanced ? {balanced}')
+
+par_checker('()()()()()()()((()))')
